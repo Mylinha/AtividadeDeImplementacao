158c6266e3da2073c9bd167af1ed33148c722ca4
MateusIFRN <109166607+MateusIFRN@users.noreply.github.com>
2022-11-21 13:47:48 -0300

    Update Main.py

--- Atividade 1/src/code/Main.py
+++ Atividade 1/src/code/Main.py
@@ -1,27 +1,33 @@
-x = int(input(" Somar = 1\n Subitrair = 2\n Mutiplicaçao = 3\n divisao = 4\n O que voçe quer?  "))
-print (x)
 
-if(x == 1):
-    num1 =  int(input("Digite um numero: "))
-    num2 =  int(input("Digite um numero: "))
+z = 's'
 
-    print(f"{num1} + {num2} = {num1 + num2}")
+while(z == 's'):
 
-elif(x == 2):
-    num1 =  int(input("Digite um numero: "))
-    num2 =  int(input("Digite um numero: "))
+    x = int(input(" Somar = 1\n Subitrair = 2\n Mutiplicaçao = 3\n divisao = 4\n O que voçe quer?  "))
 
-    print(f"{num1} - {num2} = {num1 - num2}")
-elif(x == 3):
-    num1 =  int(input("Digite um numero: "))
-    num2 =  int(input("Digite um numero: "))
+    if(x == 1):
+        num1 =  int(input("Digite um numero: "))
+        num2 =  int(input("Digite um numero: "))
 
-    print(f"{num1} x {num2} = {num1 * num2}")
-elif(x == 4):
-    num1 =  int(input("Digite um numero: "))
-    num2 =  int(input("Digite um numero: "))
+        print(f"{num1} + {num2} = {num1 + num2}")
 
-    print(f"{num1} / {num2} = {num1 / num2}")
-else:
-    print("Opçao nao existente")
-    
+    elif(x == 2):
+        num1 =  int(input("Digite um numero: "))
+        num2 =  int(input("Digite um numero: "))
+
+        print(f"{num1} - {num2} = {num1 - num2}")
+    elif(x == 3):
+        num1 =  int(input("Digite um numero: "))
+        num2 =  int(input("Digite um numero: "))
+
+        print(f"{num1} x {num2} = {num1 * num2}")
+    elif(x == 4):
+        num1 =  int(input("Digite um numero: "))
+        num2 =  int(input("Digite um numero: "))
+
+        print(f"{num1} / {num2} = {num1 / num2}")
+
+    else:
+
+        print("Opçao nao existente")
+    z = input('Digite "s" para continuar' )   
