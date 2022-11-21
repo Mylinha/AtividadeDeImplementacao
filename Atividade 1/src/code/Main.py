2ec5b4a2a8857a294752eff95ec84a4931a1de0b
MateusIFRN <109166607+MateusIFRN@users.noreply.github.com>
2022-11-21 13:33:30 -0300

    Update Main.py

---
+++ Atividade 1/src/code/Main.py
@@ -0,0 +1,14 @@
+x = int(input(" Somar = 1\n Subitrair = 2\n Mutiplicaçao = 3\n divisao = 4\n O que voçe quer?  "))
+print (x)
+
+if(x == 1):
+    num1 =  int(input("Digite um numero: "))
+    num2 =  int(input("Digite um numero: "))
+
+    print(f"{num1} + {num2} = {num1 + num2}")
+
+if(x == 2):
+    num1 =  int(input("Digite um numero: "))
+    num2 =  int(input("Digite um numero: "))
+
+    print(f"{num1} - {num2} = {num1 - num2}")
